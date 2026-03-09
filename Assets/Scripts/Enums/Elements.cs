1ac31a897461bc7df496b0ad095c2ea99660a3be
Alkaniel <contact@enzo-giardinelli.fr>
2026-03-09 22:43:59 +0100

    Gitignore

---
+++ Assets/Scripts/Enums/Elements.cs
@@ -0,0 +1,18 @@
+﻿namespace Enums
+{
+    public enum Elements
+    {
+        None,
+        Fire,
+        Water,
+        Earth,
+        Air,
+        Plasma,
+        Magmatic,
+        Plasmatic,
+        Vapor,
+        Mud,
+        Ice,
+        Sand
+    }
+}
